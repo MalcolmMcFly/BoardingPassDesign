e8fab608e9eb9506411b83349e215a18e26b7b67
Sobhit Kumar <sobhit2010@gmail.com>
2017-12-30 18:37:57 +0530

    Add class for fake boarding pass data

--- app/src/main/java/com/kumar/mrdroid/myboardingpassdesign/MainActivity.java
+++ app/src/main/java/com/kumar/mrdroid/myboardingpassdesign/MainActivity.java
@@ -1,5 +1,6 @@
 package com.kumar.mrdroid.myboardingpassdesign;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -12,6 +13,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        mainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+
     }
 }
